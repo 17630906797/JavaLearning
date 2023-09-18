c397deca4f87188a7aa1bbff7213c4e2b5b4163b
17630906797 <2274465637@qq.com>
2023-09-18 14:00:43 +0800

    push commit test2

---
+++ Day07-API-app/src/com/heima/api/Productions.java
@@ -0,0 +1,10 @@
+package com.heima.api;
+
+public class Productions {
+          public static void main(String[] args) {
+                    // 1.创建一个商品实体类
+                    FoodOperator FO = new FoodOperator();
+                    FO.addfood();
+                    FO.display();
+          }
+}
